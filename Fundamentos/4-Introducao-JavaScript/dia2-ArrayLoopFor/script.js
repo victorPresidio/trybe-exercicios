31245f0cf841c2365a52bbc9663b6f6354eb812a
Victor Presidio <victorcpresidio@gmail.com>
2022-07-21 17:01:47 -0300

    conclui o exercicio 3

--- Fundamentos/4-Introducao-JavaScript/dia2-ArrayLoopFor/script.js
+++ Fundamentos/4-Introducao-JavaScript/dia2-ArrayLoopFor/script.js
@@ -1,12 +1,8 @@
-//Exercicio 1
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for(let index = 0; index < numbers.length; index += 1){
-    console.log(numbers[index])
-}
-//Exercicio 2
-let result = 0;
+//Exercicio 3
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+let result = 0
 for(let index = 0; index < numbers.length; index += 1) {
     result += numbers[index]
 }
-console.log(result)
+let medArit = result / numbers.length
+console.log(medArit)
