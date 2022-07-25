2ab4a53adb4c2bd1c34c36be52ba3328daa6074a
Victor Presidio <victorcpresidio@gmail.com>
2022-07-25 20:26:33 -0300

    finaliza exercicio 6

--- Fundamentos/4-Introducao-JavaScript/dia-4/script.js
+++ Fundamentos/4-Introducao-JavaScript/dia-4/script.js
@@ -1,24 +1,14 @@
-let info = {
-        personagem: 'Margarida',
-        origem: 'Pato Donald',
-        nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-        recorrente: 'Sim',
-      };
 
-let info2 = {
-        personagem: 'Tio Patinhas',
-        origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-        nota: 'O último MacPatinhas',
-        recorrente: 'Sim',
-      };
-      for (let properties in info) {
-        if (
-            properties === 'recorrente' && 
-            info[properties] === 'Sim' &&
-            info2[properties] === 'Sim'
-            ) {
-            console.log('Ambos recorrentes');
-            } else {
-            console.log(info[properties] + ' e ' + info2[properties]);
-        }
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
       }
+    ]
+  };
+  console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo)
