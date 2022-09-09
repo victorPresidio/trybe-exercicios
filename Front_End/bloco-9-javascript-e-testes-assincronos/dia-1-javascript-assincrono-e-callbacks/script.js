bbc5e24924a5736375063f3ce0f6a93d49e2c5f5
Victor Presidio <victorcpresidio@gmail.com>
2022-09-09 20:06:21 -0300

    Finaliza o exercício 6

--- Front_End/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ Front_End/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -9,9 +9,9 @@ const getPlanet = () => {
     setTimeout(() => console.log('Returned planet: ', mars), 4000);
   };
   
-  getPlanet(); // Imprime Marte depois de 4 segundos
+  getPlanet(); // Imprime Marte depois de 4 segundos */
 
-  //EXERCÍCIO 4-
+//EXERCÍCIO 4-
   const messageDelay = () => Math.floor(Math.random() * 5000);
 
 const getMarsTemperature = () => {
@@ -24,4 +24,30 @@ const sendMarsTemperature = () => {
     const getTemperature = getMarsTemperature();
     setTimeout(() => console.log(`A temperatura de Marte é: ${getTemperature} graus celsius`), messageDelay());
 }
-sendMarsTemperature(); // Imprime "A temperatura de Marte é: 20 graus celsius", por exemplo
+sendMarsTemperature(); // Imprime "A temperatura de Marte é: 20 graus celsius", por exemplo */
+
+//Exercicio 5
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`Atualmente está ${toFahrenheit(temperature)}ºF em Marte`);
+
+const greet = (temperature) =>
+  console.log(`Olá! Curiosity aqui. Nesse momento está ${temperature}ºC em Marte`);
+
+// Definição da função sendMarsTemperature...
+const sendMarsTemperature = (onSuccess) => {
+    const getTemperature = getMarsTemperature();
+    setTimeout(() => onSuccess(getTemperature, messageDelay()));
+};
+
+sendMarsTemperature(temperatureInFahrenheit); // Imprime "Atualmente está 46.4ºF em Marte", por exemplo
+
+sendMarsTemperature(greet); // Imprime "Olá! Curiosity aqui. Nesse momento são 36ºC em Marte", por exemplo
