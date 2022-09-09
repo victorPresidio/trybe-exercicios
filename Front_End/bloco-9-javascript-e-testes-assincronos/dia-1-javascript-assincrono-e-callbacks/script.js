37419386d6cf2729d329b94af3b99644758328e1
Victor Presidio <victorcpresidio@gmail.com>
2022-09-09 19:45:51 -0300

    Finaliza o exercício 4

--- Front_End/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ Front_End/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -10,3 +10,18 @@ const getPlanet = () => {
   };
   
   getPlanet(); // Imprime Marte depois de 4 segundos
+
+  //EXERCÍCIO 4-
+  const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => {
+    const getTemperature = getMarsTemperature();
+    setTimeout(() => console.log(`A temperatura de Marte é: ${getTemperature} graus celsius`), messageDelay());
+}
+sendMarsTemperature(); // Imprime "A temperatura de Marte é: 20 graus celsius", por exemplo
